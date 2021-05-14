fb72a5ce9d823dbbc3483c634418a6375ce44331
VinoXie <957222673@qq.com>
2021-05-14 17:07:36 +0800

    feat:主程序类提交

--- gulimall-cart/src/main/java/com/vino/cart/CartApplication.java
+++ gulimall-cart/src/main/java/com/vino/cart/CartApplication.java
@@ -1,13 +1,18 @@
 package com.vino.cart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Vino
  * @version 1.0.0
  * @since 1.0.0
  */
 
+@SpringBootApplication
 public class CartApplication {
     public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
 
     }
 }
