8483dbebc98a609e957cdf9db8270508666b589b
VinoXie <957222673@qq.com>
2021-05-14 16:16:44 +0800

    feat: 新建网关模块

---
+++ gateway/src/main/java/com/vino/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.vino.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vino
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
